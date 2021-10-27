9497f1c8dcc5759f282f0a59da3e82c80b3edf58
jameswasnothear <92533964+jameswasnothear@users.noreply.github.com>
2021-10-27 12:54:01 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,41 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "slayer static position":return img`
+........77777.......
+.......77bccc7......
+.......7bb7777......
+.......2776616......
+.......7bb7661..b77.
+.......775bccc.b77fb
+.......b777cc7.bb77.
+........bbbbbb..f...
+....777bb77b72b777..
+...777db777b77bd777.
+.....dd.b77b77bdd...
+.....eb.bbcbcb.ee7..
+.....be.cc7c7f.ee77.
+.....eb.b7bcb7.ee77.
+.....cc.77ccc7.ccb..
+........fccbccf..c..
+.......777..777..b..
+........fc..cf...c..
+........bcb.bcb..b..
+........cbc.cbc.....
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,1 @@
-
+let the_slayer = sprites.create(assets.image`slayer static position`, SpriteKind.Player)
