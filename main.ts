85765f2ea442a31d7e919645ceea5c84a110761d
jameswasnothear <92533964+jameswasnothear@users.noreply.github.com>
2021-10-28 09:16:05 -0700

    name

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-let mySprite: Sprite = null
 let the_slayer = sprites.create(assets.image`slayer static position`, SpriteKind.Player)
 scene.setBackgroundImage(assets.image`hell on earth cityscape`)
 controller.moveSprite(the_slayer)
 scroller.setCameraScrollingMultipliers(1, 1)
 characterAnimations.loopFrames(
-mySprite,
+the_slayer,
 assets.animation`slayer concept animations`,
 500,
 characterAnimations.rule(Predicate.NotMoving)
